d4cea140aa9156469c7c4f9ee1d68c70d76ff741
Kevan Swanberg <kevswanberg@gmail.com>
2019-11-11 03:59:40 -0500

    blogpost model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
+from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
 
 
 class BlogIndexPage(Page):
@@ -10,3 +12,12 @@ class BlogIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
+
+
+class BlogPostPage(Page):
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('content', classname="full")
+    ]
+    
