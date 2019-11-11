a29a8cf9a6a211c5478c61bda33fdb9c0176a394
Kevan Swanberg <kevswanberg@gmail.com>
2019-11-11 03:46:26 -0500

    Image in jumbotron on the homepage

--- home/models.py
+++ home/models.py
@@ -1,16 +1,23 @@
-
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
-
-
-
 from wagtail.core import blocks
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    info = RichTextField()
+    info = models.CharField(max_length=140)
+    
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='home_pages'
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('info', classname='full')
+        FieldPanel('info'),
+        ImageChooserPanel('banner_image')
     ]
