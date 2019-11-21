bcdf2ab03f60d02036ee3cf66f78ce541ee4e2b6
Kevan Swanberg <kevswanberg@gmail.com>
2019-11-20 19:28:14 -0500

    bucket from conf

--- kevans_site/settings.py
+++ kevans_site/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = '<YOUR BUCKET NAME>'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
