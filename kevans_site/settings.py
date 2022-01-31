1d3aad783de196642636ded3b6fbd456b1bb3ebd
Kevan Swanberg <kevswanberg@gmail.com>
2022-01-30 19:26:14 -0500

    Update wagtail site stuff

--- kevans_site/settings.py
+++ kevans_site/settings.py
@@ -158,18 +158,13 @@ USE_TZ = True
 
 
 
-S3_USE_SIGV4 = True
-
-
-
-
-
 if DEBUG:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
 else:
+    S3_USE_SIGV4 = True
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', '')
